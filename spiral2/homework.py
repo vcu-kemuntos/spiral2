9c4d438e4f0551d3928df782f947d6ab4f13639b
Stacy Kemunto <kemuntos@vcu.edu>
2021-04-30 23:26:23 -0400

    4

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,5 +1,10 @@
 def spiralize(size, n=1):
-    """ Your code goes somewhere in here"""
-    n+= sum(4*i*i-6*(i-1)for i in range (3,size+1,2))
+    """Your code goes somewhere in here"""
+    size = 501
+    n = x == 1
+    for i in range(2, size, 2):
+        for j in range(4):
+            x += i
+            n += x
     return_value = n
     return return_value
