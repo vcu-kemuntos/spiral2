6b6d34f0bc05ac8acc8d0a4772cf8f112d74d1b7
Stacy Kemunto <kemuntos@vcu.edu>
2021-05-01 01:11:12 -0400

    6

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,7 +1,15 @@
 def spiralize(size, n=1):
     """Your code goes somewhere in here"""
-    size=501
-    for i in range(3, size+1, 2):
-        n+= 4*i*i-6*i+6
-    return_value = n
-    return return_value
+
+    num = n
+
+    for index in range(2, size, 2):
+        for reps in range(4):
+            num += index
+            n += num
+
+    return n
+
+
+
+
