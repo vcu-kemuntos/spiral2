5c24ee15b78e28ddea8468f39d33baf3e69b5410
Stacy Kemunto <kemuntos@vcu.edu>
2021-04-30 21:51:01 -0400

    round 1

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,4 +1,14 @@
 def spiralize(size, n=1):
-    """ Your code goes somewhere in here"""
-    return_value = n
-    return return_value
+    """Your code goes somewhere in here"""
+    m = [[0] * n for i in range(n)]
+    x1, y1 = [0, 1, 0, -1], [1, 0, -1, 0]
+    x, y, c = 0, -1, 1
+    for i in range(n + n - 1):
+        for j in range((n + n - i) // 2):
+            x += x1[i % 4]
+            y += y1[i % 4]
+            m[x][y] = c
+            c += 1
+    return m
+
+
